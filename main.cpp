46e58d822c9ee4e2e6fde99a758e3e51a409474b
Maicol Andrés Garzón Fonseca <55245364+MikeGarzon@users.noreply.github.com>
2020-07-05 22:00:48 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,55 +1,88 @@
-#include <cstdlib>
 #include <iostream>
-#include "listaClavesOrdenadas.h"
-using namespace std;
+#include "listaOrdenadaCentinelas.h"
 
-/*
-	INTEGRANTES:
-		MAICOL ANDRES GARZON FONSECA 20172020011
-		LUIS FELIPE CORREDOR ESPINOSA 20171020056
+using namespace std; 
 
-*/
+int main(){
+    listaord<char> c;
 
-int main(int argc, char** argv) {
+    //Verificando si la lista esta vacia -------------------------------
+    cout<<"¿Lista vacia?: ";
+    cout<<c.lista_vacia()<<endl;
+
+    //Insertando datos--------------------------------------------------
+	c.insertar(1,'A');
+    c.insertar(5,'E');
+    c.insertar(3,'C');
+    c.insertar(4,'D');
+    c.insertar(7,'G');
+    c.insertar(2,'B');
+    c.insertar(6,'F');
+
+    //Verificando si la lista esta vacia--------------------------------
+    cout<<endl<<"¿Lista vacia?: ";
+	cout<<c.lista_vacia()<<endl;
     
+    //Buscando Datos----------------------------------------------------
+    cout<<endl<<"Buscando Datos..."<<endl;
+
+    int clave = 3;
+    c.buscar(clave);
+
+    clave = 1;
+    c.buscar(clave);
+
+    clave = 7;
+    c.buscar(clave);
+
+    clave = 15;
+    c.buscar(clave);
+
+    //Eliminando Datos--------------------------------------------------
+    cout<<endl<<"Eliminando Datos..."<<endl;
+
+    clave =7;
+    c.borrar(clave);
     
-    //Insertando datos------------
-    //SE PRUEBA SOLO CON CHAR PARA NO HACERLO CONFUSO, PERO FUNCIONA CON CUALQUIER TIPO 
-	lista<char> c;
-	c.insertar(1001,'Z');
-	c.insertar(1002,'X');
-	c.insertar(1003,'Y');
-	c.insertar(1015,'M');
-	
-	//Buscando datos--------------
-	int dato = 1015; 
-	if(c.enLista(dato)){
-    cout<<"Buscando <"<<dato<<">..."<<endl;		
-	cout<<c.buscar(dato).Dato<<endl;
-	}else{
-		cout<<"Buscando <"<<dato<<">..."<<endl;	
-		cout<<"Dato no encontrado";
-	}
-	
-	cout<<"---------------------------------"<<endl;
-	//Imrimiendo datos-------------
-	c.imprimir();
-	
-	cout<<"---------------------------------"<<endl;
-	//Borrando datos
-	dato = 1003; 
-	cout<<"Borrando<"<<dato<<">..."<<endl;		
-	c.borrar(dato);
+    clave =1;
+    c.borrar(clave);
 
-    cout<<"---------------------------------"<<endl;
-    //Verificando si se borr
-	if(c.enLista(dato)){
-	cout<<c.buscar(dato).Dato<<endl;
-	}else{
-		cout<<"Buscando <"<<dato<<">..."<<endl;	
-		cout<<"Dato no encontrado";
-	}
+    clave =5;
+    c.borrar(clave);
 
-		
-	return 0;
+    clave =18;
+    c.borrar(clave);
+
+    //Pertenece--------------------------------------------------------
+    cout<<endl<<"La clave <"<<clave<<"> ¿Pertenece a la lista? :";
+    cout<<c.pertenece(clave)<<endl;
+
+    clave = 6;
+    cout<<"La clave <"<<clave<<"> ¿Pertenece a la lista? :";
+    cout<<c.pertenece(clave)<<endl;
+
+    //Imprimiendo lista-----------------------------------------------
+    cout<<endl<<"Imprimiendo lista"<<endl;
+    c.recorrer();
+
+    //Cambiando informacion ------------------------------------------
+    c.asignarInfo(2,'X');
+    c.asignarInfo(6,'Y');
+
+    cout<<endl<<"Despues de cambiar la informacion"<<endl;
+    c.buscar(2);
+    c.buscar(6);
+
+    cout<<endl<<"Acceso a nodos: (-1) significa no enlace"<<endl;
+
+    clave = 2;
+    cout<<"Clave siguiente de "<<clave<<"-> "<<c.siguiente(clave)<<endl;
+
+    clave = 6;
+    cout<<"Clave anterior de "<<clave<<"-> "<<c.anterior(clave)<<endl;
+
+    clave = 6;
+    cout<<"Clave siguiente de "<<clave<<"-> "<<c.siguiente(clave)<<endl;
+
+    return 0;
 }
